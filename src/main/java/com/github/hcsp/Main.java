12918d3c5fd9f410783ee0cb31f3ebb523a6a2a4
Bo Zhang <zhangbodut@gmail.com>
2019-10-30 20:20:30 +0800

    Finish the crawler

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -2,7 +2,7 @@ package com.github.hcsp;
 
 public class Main {
     public static void main(String[] args) {
-        CrawlerDao dao = new MyBatisCrawlerDao();
+        CrawlerDao dao = new JdbcCrawlerDao();
 
         for (int i = 0; i < 8; ++i) {
             new Crawler(dao).start();
